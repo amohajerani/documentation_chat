ba268e899e1f9de45d38461d24ea34891e47a7f8
Amir Mohajerani <amohajerani6@gmail.com>
2023-05-30 07:10:10 -0400

    none

--- app/app.py
+++ app/app.py
@@ -17,4 +17,4 @@ def get_answer():
     return jsonify(answer=answer, references=references)
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
